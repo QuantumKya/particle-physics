cd98ced43d21a80ff842d727545a10449d6cec97
QuantumKya <106355292+QuantumKya@users.noreply.github.com>
2026-02-13 14:29:40 -0600

    refactoring and changing on data processing and added more getter functions

--- hexadecimal_decimalwise.py
+++ hexadecimal_decimalwise.py
@@ -8,9 +8,8 @@ def to_decimal(hex):
 class DataLooker:
     def __init__(self, filename):
         self.datalist = []
-
-    def process_data(self):
-        with open(filename, 'r') as f:
+        self.datalines = []
+        with open(filename, 'r') as file:
             in_data = False
             for line in file:
                 linedata = line.strip()
@@ -18,35 +17,39 @@ class DataLooker:
                     in_data = True
                 elif linedata == 'CD':
                     in_data = False
-                elif not in_data:
+                
+                if not in_data:
                     continue
+                self.datalines.append(line.strip())
 
-                words = linedata.split()
-                def get_word_val(i):
-                    return to_decimal(words[i])
-                
-                data = {}
-                
-                def get_data_block(i):
-                    return {
-                        "rising": get_word_val(i),
-                        "falling": get_word_val(i+1)
-                    }
-                data["detector_data"] = map(get_data_block, [1, 3, 5, 7])
+    def process_data(self, i):
+        data = {}
 
-                data["date"] = get_word_val(11)
-                data["internal_time"] = get_word_val(0)
-                data["clock_check_time"] = {
-                    "internal": get_word_val(9),
-                    "utc": float(words[10]),
-                }
-                data["ppi_gps_time_difference"] = int(words[15])
+        words = self.datalines[i].split()
+        
+        def get_word_val(i):
+            return to_decimal(words[i])
+        
+        def get_data_block(i):
+            return {
+                "rising": get_word_val(i),
+                "falling": get_word_val(i+1)
+            }
+        data["detector_data"] = map(get_data_block, [1, 3, 5, 7])
 
-                data["valid_gps"] = ({ "A": True, "V": False })[words[12]]
-                data["sattelite_count"] = int(words[13])
-                data["errors"] = int(words[14])
+        data["date"] = get_word_val(11)
+        data["internal_time"] = get_word_val(0)
+        data["clock_check_time"] = {
+            "internal": get_word_val(9),
+            "utc": float(words[10]),
+        }
+        data["ppi_gps_time_difference"] = int(words[15])
 
-                self.datalist.append(data)
+        data["valid_gps"] = ({ "A": True, "V": False })[words[12]]
+        data["sattelite_count"] = int(words[13])
+        data["errors"] = int(words[14])
+
+        self.datalist.append(data)
 
     def get_data(self, datum_index):
         return self.datalist[datum_index]
@@ -56,6 +59,19 @@ class DataLooker:
 
     def was_error(self, datum_index):
         return self.get_data(datum_index)["errors"] > 0
+
+    def was_valid_gps(self, datum_index):
+        return self.get_data(datum_index)["valid_gps"]
+
+    def get_time_data(self, datum_index):
+        datum = self.get_data(datum_index)
+        
+        out = {}
+        for key in ["date", "internal_time", "ppi_gps_time_difference"]:
+            out[key] = datum[key]
+        out["gps_clock_check"] = datum["clock_check_time"]
+        
+        return out
 
 
 decoder = DataLooker("EQUIP_12FEB2026_161338.txt")
